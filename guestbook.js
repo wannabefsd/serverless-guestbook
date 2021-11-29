4be0d969bc7bb06f20b58a2fcd680a2a8f72b8fe
wannabefsd <91594754+wannabefsd@users.noreply.github.com>
2021-11-29 14:09:59 +0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://89103b67.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
